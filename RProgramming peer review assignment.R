c57c419e5aa141a79ccbccaf73dc3e7153ed565b
jliu1231 <jialuliu1231@gmail.com>
2021-01-31 21:07:00 -0600

    Add files via upload

---
+++ RProgramming peer review assignment.R
@@ -0,0 +1,33 @@
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+cacheSolve <- function(x, ...) {
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
+
+test <- matrix(c(1,2,3,4),2,2)
+M <- makeCacheMatrix(test)
+cacheSolve(M)
+
+
